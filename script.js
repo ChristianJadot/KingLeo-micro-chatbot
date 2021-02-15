4b1c02588f17e08d88e86ffad8ac8f8cebc94264
guillaume-leo <quet.guillaume.leo@gmail.com>
2021-02-15 12:19:48 +0100

    shift+Enter trick added

--- script.js
+++ script.js
@@ -17,22 +17,35 @@ var lastUserMessage = "";
 
 function myFunction(event) {
     var x = event.which || event.keyCode;
+    console.log(x);
     var shiftKeyPressed = event.shiftKey;
-    console.log(shiftKeyPressed);
-    if (x==13){
-        lastUserMessage = document.getElementById('userBox').value;
-        historyOfMessages.push(lastUserMessage);
-        document.getElementById('userBox').value = "";
+    if (x==13 && shiftKeyPressed == true){null;}
+        else if (x==13){
+            userBox = document.getElementById('userBox');
+            lastUserMessage = userBox.value;
+            historyOfMessages.push(lastUserMessage);
+
+        
+
+        userBox.value="hello you"; //userBox.defaultValue;
+        
         newChatBox = document.createElement("textArea");
         newChatBox.setAttribute("type", "text");
         newChatBox.setAttribute("id", "bulle");
         newChatBox.style.display = "block";
+        newChatBox.style.overflow = "hidden";
         newChatBox.style.textAlign = "right";
-        // newChatBox.setAttribute("value", lastUserMessage);
         newChatBox.value = lastUserMessage;
         newChatBox.disabled = true;
+        nbOfLines = lastUserMessage.split('\n').length * 16.8; 
+        nbOfLinesToString = nbOfLines.toString()+"px";
+        userBox.setSelectionRange(10,0,"backward");
+
+
+        newChatBox.style.height = nbOfLinesToString;
+
         document.getElementById('containerBulles').appendChild(newChatBox);
-        console.log(lastUserMessage);
+        
     }
   }
 
