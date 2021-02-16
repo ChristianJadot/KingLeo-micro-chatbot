cbeef75a5f78e2ae173d7341f5313d9ae75dea39
guillaume-leo <quet.guillaume.leo@gmail.com>
2021-02-16 09:04:18 +0100

    regex ok

--- script.js
+++ script.js
@@ -6,33 +6,33 @@
 //! end loop----------
 
 //welcome messages + random message
-let welcomeMessages = ["Hi there buddy! How are You today?","Hey You! How's it going?","Well helo there! Tell me, How have you been?"];
+let welcomeMessages = ["Hi there buddy! How are You today?", "Hey You! How's it going?", "Well helo there! Tell me, How have you been?"];
 
-const randomIndex = Math.round(Math.random()*welcomeMessages.length);
+const randomIndex = Math.round(Math.random() * welcomeMessages.length);
 
 
 console.log(welcomeMessages[randomIndex]);
 
 //bot cannot understand + randomiser
 
-let endMessages = ["Try again please, my robot-ears aren't what they used to be!","I do not understand! Please repeat!","What kind of language are you speaking? In English please!"];
+let endMessages = ["Try again please, my robot-ears aren't what they used to be!", "I do not understand! Please repeat!", "What kind of language are you speaking? In English please!"];
 
-const randomIndex2 = Math.round(Math.random()*endMessages.length);
+const randomIndex2 = Math.round(Math.random() * endMessages.length);
 
 
 
 // yes + randomiser
 
-let YesAnwsers = ["Well that's good to hear! Keep it up!","Nice! Enjoying the good life right!","That's the way to do it!"];
+let YesAnwsers = ["Well that's good to hear! Keep it up!", "Nice! Enjoying the good life right!", "That's the way to do it!"];
 
-const randomIndex3 = Math.round(Math.random()*YesAnwsers.length);
+const randomIndex3 = Math.round(Math.random() * YesAnwsers.length);
 
 
 //no + randomiser 
 
-let NoAnwsers =["Don't worry to much. It will become better.","Sometime life sucks but i won't stay that way I promise.","I'm sorry I hope it gets better soon."];
+let NoAnwsers = ["Don't worry to much. It will become better.", "Sometime life sucks but i won't stay that way I promise.", "I'm sorry I hope it gets better soon."];
 
-const randomIndex4 = Math.round(Math.random()*NoAnwsers.length);
+const randomIndex4 = Math.round(Math.random() * NoAnwsers.length);
 
 // goodbye
 
@@ -41,17 +41,37 @@ let byeMessage = "See you around";
 // bot answers
 
 function robotAnswers() {
-  
-    if ( lastUserMessage.search(/\b(good|fine|top|very good)\b/i)) {
+
+    var positiveWords = /"can't complain"|great|"not bad"|"not so bad"|good|top|fine|"very good"|yes/g;
+    var negativeWords = /bad|not|shitty|shit|not good|no/g;
+
+
+
+
+    isPositive = lastUserMessage.match(positiveWords) == null ? [] : lastUserMessage.match(positiveWords).length;
+    isNegative = lastUserMessage.match(negativeWords) == null ? [] : lastUserMessage.match(negativeWords).length;
+
+
+console.log(lastUserMessage.match(positiveWords));
+console.log(lastUserMessage.match(negativeWords));
+
+console.log(isNegative);
+
+
+
+    // console.log(lastUserMessage.match(positiveWords)!=null);
+
+
+    if (isPositive>isNegative) {
         console.log(YesAnwsers[randomIndex3])
     }
-    else if ( lastUserMessage.search(/\b(bad|not|shitty|shit)\b/i)) {
-        console.log(NoAnwsers[randomIndex3])}
-    
-    else  {console.log(endMessages[randomIndex2])};
+    else if (isNegative<isPositive) {
+        console.log(NoAnwsers[randomIndex3])
+    }
+    else { console.log(endMessages[randomIndex2]) };
 
 
-}    
+}
 
 
 
@@ -73,39 +93,38 @@ var lastUserMessage = "";
 function userSpeak(event) {
     var x = event.which || event.keyCode;
     var shiftKeyPressed = event.shiftKey;
-    if (x==13 && shiftKeyPressed == true) // Shift-Enter 
-    {null;}
-        else if (x==13){ // Just Enter
-            // assign the user textArea to a variable "userBox"
-            userBox = document.getElementById('userBox');
-            // get the value of it and assign that text to "lastUserMessage"
-            lastUserMessage = userBox.value;
-            // push this value into "historyOfMessages"
-            historyOfMessages.push(lastUserMessage);
+    if (x == 13 && shiftKeyPressed == true) // Shift-Enter 
+    { null; }
+    else if (x == 13) { // Just Enter
+        // assign the user textArea to a variable "userBox"
+        userBox = document.getElementById('userBox');
+        // get the value of it and assign that text to "lastUserMessage"
+        lastUserMessage = userBox.value;
+        // push this value into "historyOfMessages"
+        historyOfMessages.push(lastUserMessage);
 
-        
-            // Clear the userBox
-            $('#userBox').val('');
-        
-            // BUBBLE CREATION with some properties
-            newChatBox = document.createElement("textArea");
-            newChatBox.setAttribute("type", "text");
-            newChatBox.setAttribute("id", "bulle");
-            newChatBox.style.display = "block";
-            newChatBox.style.overflow = "hidden";
-            newChatBox.style.textAlign = "right";
-            newChatBox.value = lastUserMessage;
-            newChatBox.disabled = true;
-            // get the number of lines to set the height of the bubble
-            nbOfLines = (lastUserMessage.split('\n').length-1) * 16.8; 
-            nbOfLinesToString = nbOfLines.toString()+"px";
-            // set the height of the bubble
-            newChatBox.style.height = nbOfLinesToString;
-            // add this bubble to the html document
-            document.getElementById('containerBulles').appendChild(newChatBox);
-            robotAnswers();
+
+        // Clear the userBox
+        $('#userBox').val('');
+
+        // BUBBLE CREATION with some properties
+        newChatBox = document.createElement("textArea");
+        newChatBox.setAttribute("type", "text");
+        newChatBox.setAttribute("id", "bulle");
+        newChatBox.style.display = "block";
+        newChatBox.style.overflow = "hidden";
+        newChatBox.style.textAlign = "right";
+        newChatBox.value = lastUserMessage;
+        newChatBox.disabled = true;
+        // get the number of lines to set the height of the bubble
+        nbOfLines = (lastUserMessage.split('\n').length - 1) * 16.8;
+        nbOfLinesToString = nbOfLines.toString() + "px";
+        // set the height of the bubble
+        newChatBox.style.height = nbOfLinesToString;
+        // add this bubble to the html document
+        document.getElementById('containerBulles').appendChild(newChatBox);
+        robotAnswers();
 
     }
-  }
+}
 
-  
